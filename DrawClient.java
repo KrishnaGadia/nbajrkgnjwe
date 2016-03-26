75b757a838378a630e795900395353f655328452
Krishna Gadia <krishnagadia3@gmail.com>
2016-03-26 18:02:57 -0400

    Added files via upload
    Basic layout..one client one server.

---
+++ DrawClient.java
@@ -0,0 +1,62 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class DrawClient
+{
+	static int portNo = 6789;
+	public static void main(String args[]) throws IOException
+	{
+		if (args.length != 1) {
+		    System.err.println(
+		        "Usage: java EchoClient <host name>");
+		    System.exit(1);
+	        }
+		String hostName = args[0];
+        
+		try(
+			Socket echoSocket = new Socket(hostName, portNo);
+		    PrintWriter outToServer =
+			new PrintWriter(echoSocket.getOutputStream(), true);
+		    BufferedReader inFromServer =
+			new BufferedReader(
+			    new InputStreamReader(echoSocket.getInputStream()));
+		    BufferedReader stdIn =
+			new BufferedReader(
+			    new InputStreamReader(System.in));
+		) {
+            System.out.println("Please Enter \n1 if you are Coordinator \n2 if you are reciever");
+            int choice = Integer.parseInt(stdIn.readLine());
+        
+            if(choice == 1){
+                String userInput;
+                String ServerInput="";
+                while ((userInput = stdIn.readLine()) != null ) {
+                    outToServer.println(userInput);
+                    ServerInput = inFromServer.readLine();
+                    System.out.println("echo: " + ServerInput);
+                    if(ServerInput.charAt(0)=='.')
+                        break;
+                }
+                System.out.println(" You entered \".\", hence session ended ");
+            }
+            else{
+                String ServerInput="";
+                while ((ServerInput = inFromServer.readLine()) != null ) {
+                    System.out.println("echo: " + ServerInput);
+                    if(ServerInput.charAt(0)=='.')
+                        break;
+                }
+                System.out.println(" The co-ordinator entered \".\", hence session ended ");
+            
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
+}
---
+++ DrawServer.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class DrawServer 
+{
+    
+	static int portNo = 6789;
+	public static void main(String args[]) 
+	{	
+		InputStream is ;
+		OutputStream os ;
+        String record = "";//Keeps a record of the things obtained till now
+		try{
+		
+		ServerSocket ss = new ServerSocket(portNo);
+		Socket cs = ss.accept();
+		PrintWriter outToClient = new PrintWriter(cs.getOutputStream(),true);
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+		
+
+		String inputLine;
+	    while ((inputLine = inFromClient.readLine()) != null ) {
+            record = record.concat(inputLine+"\n");
+            System.out.println(inputLine+" got");
+	        outToClient.println(inputLine);
+            if(inputLine.charAt(0)=='.')
+                    break;
+	    }
+
+		System.out.println(record); 
+		}
+
+		catch( Exception e){
+			System.out.println(e.toString()+" is the error");
+        }
+	}
+}
+
